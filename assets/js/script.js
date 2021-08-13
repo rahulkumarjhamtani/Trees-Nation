a84fa58cd05a3e806d9fa123d5a68bc09623ff68
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-08-13 21:04:03 +0530

    trees nation

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var burger = document.getElementsByClassName('navbar-burger')[0];
+    burger.addEventListener('click', (e) => {
+        var navbar = document.getElementById(burger.dataset.target);
+        navbar.classList.toggle('is-active');
+        burger.classList.toggle('is-active');
+    });
+
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+
+    document. getElementById("btn"). onclick = function () {
+        location. href = "donate.html";
+        };
